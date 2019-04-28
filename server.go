0f144b7d7a3c32f204b5bf737e4b0ab2b457ba9b
Yves Lelievre <lelievre.yves@gmail.com>
2019-04-28 23:15:14 +0200

    Move data to its folder

--- server.go
+++ server.go
@@ -6,7 +6,6 @@ import (
     "io/ioutil"
     "html/template"
     "regexp"
-    "errors"
 )
 
 /*** Globals ***/
@@ -21,7 +20,7 @@ type Page struct {
 
 /*** Utils ***/
 func loadPage(title string) (*Page, error) {
-    filename := title + ".txt"
+    filename := "data/" + title + ".txt"
     body, err := ioutil.ReadFile(filename)
     if err != nil {
         return nil, err
@@ -30,25 +29,16 @@ func loadPage(title string) (*Page, error) {
     return &Page{Title: title, Body: body}, nil
 }
 
+func (p *Page) save() error {
+    filename := "data/" + p.Title + ".txt"
+    return ioutil.WriteFile(filename, p.Body, 0600)
+}
+
 func renderTemplate(filename string, w http.ResponseWriter, p *Page) {
     err := templates.ExecuteTemplate(w, filename + ".html", p)
     if err != nil {
         http.Error(w, err.Error(), http.StatusInternalServerError)
     }
-}
-
-func (p *Page) save() error {
-    filename := p.Title + ".txt"
-    return ioutil.WriteFile(filename, p.Body, 0600)
-}
-
-func getTitle(w http.ResponseWriter, r *http.Request) (string, error) {
-    m := validPath.FindStringSubmatch(r.URL.Path)
-    if m == nil {
-        http.NotFound(w, r)
-        return "", errors.New("Invalid page title")
-    }
-    return m[2], nil
 }
 
 /*** Route handlers ***/
